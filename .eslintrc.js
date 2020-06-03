461d688d108062aa04bc8926552d9c4d3a305aa0
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2020-06-03 19:41:35 +0300

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: [
+    'xo',
+    'xo-typescript',
+    'xo-react'
+  ],
+};
---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Index = () => (
+	<div className="indexPage">
+		<div className="intro">
+			<h1>
+				he11o.<br/>I am Charalampos Fanoulis
+			</h1>
+			<p>
+				I&apos;m a 15 year old selftaught developer,<br/>{}
+				currently attending the 6th Junior High of Evosmos,<br/>{}
+				born and raised in Thessaloniki, GR
+
+				{}<br/>
+				{}<br/>{}
+
+				I&apos;m experienced with TypeScript and JavaScript,<br/>{}
+				as well as some Rust and Python.
+			</p>
+		</div>
+	</div>
+);
+
+export default Index;
