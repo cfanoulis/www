d06b45c7d7113175595d97be79474c36be090c2b
Charalampos Fanoulis <yo@fanoulis.dev>
2020-09-12 19:41:20 +0300

    fix: whooops

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,23 +3,29 @@ import React, {useEffect, useState} from 'react';
 import styles from '../stylesheets/pages/projects.module.scss';
 
 const fetchProjects = () => {
-	return [{name: 'Skyra', description: ' Skyra is a robust All-in-One bot for Discord', link: [
-		{name: 'Visit Skyra', link: 'https://skyra.pw'},
-		{name: 'Skyra on GitHub', link: 'https://github.com/skyra-project/skyra'}
-
-	]}];
+	return [
+		{
+			name: 'Skyra', description: ' Skyra is a robust All-in-One bot for Discord', links: [
+				{name: 'Visit Skyra', link: 'https://skyra.pw'},
+				{name: 'Skyra on GitHub', link: 'https://github.com/skyra-project/skyra'}
+			]
+		}
+	];
 };
 
 const generateCards = () => {
 	const projects = fetchProjects();
-	return projects.map(e => {
+	return projects.map(project => {
 		return (
-			<div key={e.name} className={styles.card}>
-				<p className={styles.cardtitle}>{e.name}</p>
-				<p className={styles.carddesc}>{e.description}</p>
-				{e.link.map(linkElement => {
+			<div key={project.name} className={styles.card}>
+				<p className={styles.cardtitle}>{project.name}</p>
+				<p className={styles.carddesc}>{project.description}</p>
+				{project.links.map(linkElement => {
 					return (
-						<a key={linkElement.name} className={styles.carddesc} href={linkElement.link} target="_blank" rel="noreferrer">{linkElement.name}</a>
+						<div key={linkElement.name} className={styles.cardlink} >
+							<br />
+							<a href={linkElement.link} target="_blank" rel="noreferrer">{linkElement.name}</a>
+						</div>
 					);
 				})}
 			</div>
