ce973d7bbff48ce500ad22eaa191efabaa603c9a
Charalampos Fanoulis <yo@fanoulis.dev>
2021-10-13 20:33:55 +0300

    refactor: remove apollo provider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,12 @@
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web';
-import { RedwoodApolloProvider } from '@redwoodjs/web/apollo';
 import FatalErrorPage from 'src/pages/FatalErrorPage';
 import Routes from 'src/Routes';
 import './index.css';
-import './i18n'
 
 const App = () => (
 	<FatalErrorBoundary page={FatalErrorPage}>
 		<RedwoodProvider titleTemplate="%PageTitle">
-			<RedwoodApolloProvider>
-				<Routes />
-			</RedwoodApolloProvider>
+			<Routes />
 		</RedwoodProvider>
 	</FatalErrorBoundary>
 );
