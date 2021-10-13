ff37507b9c27602159f3c589337b6243037e50ea
Charalampos Fanoulis <yo@fanoulis.dev>
2021-10-13 20:31:04 +0300

    feat: setup tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,18 @@
-import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
-import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
-
-import FatalErrorPage from 'src/pages/FatalErrorPage'
-import Routes from 'src/Routes'
-
-import './index.css'
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web';
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo';
+import FatalErrorPage from 'src/pages/FatalErrorPage';
+import Routes from 'src/Routes';
+import './index.css';
+import './i18n'
 
 const App = () => (
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
-    </RedwoodProvider>
-  </FatalErrorBoundary>
-)
+	<FatalErrorBoundary page={FatalErrorPage}>
+		<RedwoodProvider titleTemplate="%PageTitle">
+			<RedwoodApolloProvider>
+				<Routes />
+			</RedwoodApolloProvider>
+		</RedwoodProvider>
+	</FatalErrorBoundary>
+);
 
-export default App
+export default App;
