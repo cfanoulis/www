5eeedf964090d19df5718a90b5367a4c49550abf
Charalampos Fanoulis <yo@fanoulis.dev>
2021-12-09 19:04:17 +0200

    stuff

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,14 @@
-import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
-import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
-
-import FatalErrorPage from 'src/pages/FatalErrorPage'
-import Routes from 'src/Routes'
-
-import './index.css'
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web';
+import FatalErrorPage from 'src/pages/FatalErrorPage';
+import Routes from 'src/Routes';
+import './index.css';
 
 const App = () => (
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
-    </RedwoodProvider>
-  </FatalErrorBoundary>
-)
+	<FatalErrorBoundary page={FatalErrorPage}>
+		<RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+			<Routes />
+		</RedwoodProvider>
+	</FatalErrorBoundary>
+);
 
-export default App
+export default App;
