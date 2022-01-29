51a163299f144e4eb0c850e36a79dcfa36b99811
Charalampos Fanoulis <yo@fanoulis.dev>
2022-01-29 09:56:44 +0200

    chore: upload progress on projectcard

--- web/src/components/ProjectCard/ProjectCard.tsx
+++ web/src/components/ProjectCard/ProjectCard.tsx
@@ -10,7 +10,7 @@ const ProjectCard = ({ projectTitle, projectDescription, current, img }: Project
 		<div className={`glass p-8 rounded lg:max-w-md ${current ? 'order-1' : ''}`}>
 			<h5 className="text-xl font-heading font-bold">{projectTitle}</h5>
 			<p className="text-base font-sans font-normal">{projectDescription}</p>
-			<img src="https://github.com/skyra-project.png" />
+			<img src="https://github.com/skyra-project.png" alt="" />
 		</div>
 	);
 };
