0857782811d112fba1abc8213fa3cc13a19d183d
Charalampos Fanoulis <yo@fanoulis.dev>
2022-01-30 22:52:24 +0200

    first steps to v3.5 - new design

--- web/src/components/ProjectCard/ProjectCard.stories.tsx
+++
@@ -1,7 +0,0 @@
-import ProjectCard from './ProjectCard';
-
-export const generated = () => {
-	return <ProjectCard projectTitle="Storybook" projectDescription="Lorem ipsum dolor sit amet" current />;
-};
-
-export default { title: 'Components/ProjectCard' };
