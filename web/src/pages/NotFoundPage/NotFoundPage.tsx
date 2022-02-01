3c1e799609b1c5024529e3cf80a0617d8974b42b
Charalampos Fanoulis <yo@fanoulis.dev>
2022-02-01 14:16:02 +0200

    give 404page similarish design

--- web/src/pages/NotFoundPage/NotFoundPage.tsx
+++ web/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,44 +1,17 @@
+import { Link, routes } from '@redwoodjs/router';
+
 export default () => (
-	<main>
-		<style
-			dangerouslySetInnerHTML={{
-				__html: `
-              html, body {
-                margin: 0;
-              }
-              html * {
-                box-sizing: border-box;
-              }
-              main {
-                display: flex;
-                align-items: center;
-                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif;
-                text-align: center;
-                background-color: #E2E8F0;
-                height: 100vh;
-              }
-              section {
-                background-color: white;
-                border-radius: 0.25rem;
-                width: 32rem;
-                padding: 1rem;
-                margin: 0 auto;
-                box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-              }
-              h1 {
-                font-size: 2rem;
-                margin: 0;
-                font-weight: 500;
-                line-height: 1;
-                color: #2D3748;
-              }
-            `
-			}}
-		/>
-		<section>
-			<h1>
-				<span>404 Page Not Found</span>
-			</h1>
-		</section>
+	<main className="min-h-screen w-screen p-4 lg:p-16 flex flex-col bg-neutral-900 text-gray-300 absolute">
+		<h1 className="text-3xl md:text-6xl font-heading font-medium">
+			Error Four-Oh-Four:
+			<br />
+			Not the page you&apos;re looking for
+		</h1>
+		<p className="pt-4 lg:pt-14">
+			You&apos;ve landed on a page that doesn&apos;t exist. Let&apos;s take you back to the{' '}
+			<Link to={routes.landing()} className="underline">
+				home page ↗
+			</Link>
+		</p>
 	</main>
 );
