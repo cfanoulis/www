a9b9b6540aa23f07c00e2ed7336d91e64415dc2d
Charalampos Fanoulis <yo@fanoulis.dev>
2022-02-01 13:58:19 +0200

    various changes to section component

--- web/src/components/LandingPageSection/LandingPageSection.tsx
+++ web/src/components/LandingPageSection/LandingPageSection.tsx
@@ -1,10 +1,13 @@
 const LandingPageSection = ({ children, title }: { children: React.ReactNode; title: string }) => {
 	return (
-		<div className="pt-6 lg:pt-16 md:flex">
-			<div id="title-wrapper" className="md:pl-4 md:vertical-text md:rotate-180 flex md:justify-end text-xl md:text-2xl tracking-widest font-heading">
+		<div className="pt-4 lg:pt-14 md:flex w-3/4">
+			<div
+				id="title-wrapper"
+				className="md:pl-4 md:vertical-text md:rotate-180 flex md:justify-end text-xl md:text-2xl tracking-widest font-heading"
+			>
 				<h3>{title}</h3>
 			</div>
-			<p>{children}</p>
+			<p className="leading-7">{children}</p>
 		</div>
 	);
 };
