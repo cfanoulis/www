423c39be7363bb6d2f839867de18d15ceaa83439
Charalampos Fanoulis <yo@fanoulis.dev>
2022-02-15 21:59:42 +0200

    revert: wording changes made accidentally

--- web/src/pages/NotFoundPage/NotFoundPage.tsx
+++ web/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -10,13 +10,17 @@ export default () => (
 	You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
 		/>
 		<main className="min-h-screen w-screen p-4 lg:p-16 flex flex-col bg-neutral-900 text-gray-300 absolute">
-			<h1 className="text-4xl md:text-6xl font-heading font-medium">Oh, don&apos;t mind the construction work</h1>
-			<p className="pt-4 lg:pt-14">
-				This page is just under construction. You probably want to return to the{' '}
-				<Link to={routes.landing()} className="underline">
-					home page ↗
-				</Link>
-			</p>
+		<h1 className="text-4xl md:text-6xl font-heading font-medium">
+			Error Four-Oh-Four:
+			<br />
+			Not the page you&apos;re looking for
+		</h1>
+		<p className="pt-4 lg:pt-14">
+			You&apos;ve landed on a page that doesn&apos;t exist. Let&apos;s take you back to the{' '}
+			<Link to={routes.landing()} className="underline">
+				home page ↗
+			</Link>
+		</p>
 		</main>
 	</>
 );
