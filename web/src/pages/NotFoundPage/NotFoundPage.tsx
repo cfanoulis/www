a56e2e8151427e3f5bd4e0df0842e5f65f27281f
Charalampos Fanoulis <yo@fanoulis.dev>
2022-02-15 19:54:06 +0200

    make text more leglible in 404 page

--- web/src/pages/NotFoundPage/NotFoundPage.tsx
+++ web/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import { Link, routes } from '@redwoodjs/router';
 
 export default () => (
 	<main className="min-h-screen w-screen p-4 lg:p-16 flex flex-col bg-neutral-900 text-gray-300 absolute">
-		<h1 className="text-3xl md:text-6xl font-heading font-medium">
+		<h1 className="text-4xl md:text-6xl font-heading font-medium">
 			Error Four-Oh-Four:
 			<br />
 			Not the page you&apos;re looking for
