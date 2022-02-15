31167a15d4134417de5f1022bfadd5c4c3f23759
Charalampos Fanoulis <yo@fanoulis.dev>
2022-02-15 19:54:26 +0200

    projects section

--- web/src/components/LandingPageSection/LandingPageSection.tsx
+++ web/src/components/LandingPageSection/LandingPageSection.tsx
@@ -3,7 +3,7 @@ const LandingPageSection = ({ children, title }: { children: React.ReactNode; ti
 		<div className="pt-4 lg:pt-14 md:flex w-3/4">
 			<div
 				id="title-wrapper"
-				className="md:pl-4 md:vertical-text md:rotate-180 flex md:justify-end text-xl md:text-2xl tracking-widest font-heading"
+				className="font-medium md:pl-4 md:vertical-text md:rotate-180 flex md:justify-end text-xl md:text-2xl tracking-widest font-heading"
 			>
 				<h3>{title}</h3>
 			</div>
---
+++ web/src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,28 @@
+interface CardProps {
+	img?: string;
+	name: string;
+	desc: string;
+	url?: string;
+	featured?: boolean;
+}
+
+const ProjectCard = (props: CardProps) => {
+	return (
+		<div className="border-2 p-2 rounded-sm border-slate-400">
+			{props.featured ? (
+				<>
+					<a href={props.url}>
+						<h5 className="font-medium underline">{props.name} ↗</h5>
+					</a>
+				</>
+			) : (
+				<>
+					<h5 className="font-medium">{props.name}</h5>
+				</>
+			)}
+			<p className="pt-2 text-sm">{props.desc}</p>
+		</div>
+	);
+};
+
+export default ProjectCard;
